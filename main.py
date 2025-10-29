de158ffe9676db72e7e07a5d27fdce82d846bdb4
michael <michael.short094@gmail.com>
2025-10-28 22:24:30 -0600

    sjf works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from multiprocessing.process import current_process
 import sys
 
 class Process:
@@ -55,8 +56,62 @@ def first_come_first_served(inputs: list):
     return avg_response, avg_wait, avg_turnaround
 
 def shortest_job_first(inputs: list):
-    pass
+    #lists are stored as: [running_sum, counter]
+    response_times = [0, 0]
+    wait_times = [0, 0]
+    turnaround_times = [0, 0]
 
+    prossess_queue = []
+    clock = 0
+    cur_prossess = None
+    unarrived = len(inputs)
+    processed_count = 0
+
+    while(True):
+        #check for a ready prossess
+        for i in inputs:
+            if i[0] == clock:
+                ready_prossess = Process(clock, i[1])
+                prossess_queue.append(ready_prossess)
+                unarrived -= 1
+
+        #if there is no current prosses grab the shortest one
+        if not cur_prossess and prossess_queue:
+            cur_prossess = min(prossess_queue, key=lambda p: p.burst_time)
+            prossess_queue.remove(cur_prossess)
+
+        # Track first start for response time
+        if cur_prossess and cur_prossess.start_time is None:
+            cur_prossess.start_time = clock
+            response_times[0] += cur_prossess.start_time - cur_prossess.arrival_time
+            response_times[1] += 1
+
+        #incremnt one tick on the current prosses, if this ends the prosses greab its data
+        if cur_prossess:
+            cur_prossess.remaining_time -= 1
+        #if prossess is done on this cycle, complete it and add stats to return values
+        if cur_prossess and cur_prossess.remaining_time == 0:
+            completion_time = clock + 1#competes at the end of the tick
+            wait_times[0] += cur_prossess.wait_time
+            wait_times[1] += 1
+
+            turnaround_times[0] += completion_time - cur_prossess.arrival_time
+            turnaround_times[1] += 1
+            cur_prossess = None
+            processed_count += 1
+        #increment wait times and clock
+        for i in prossess_queue:
+            i.wait_time += 1
+        clock += 1
+        
+        #check for no work left
+        if processed_count == len(inputs):
+            break
+
+    avg_response = response_times[0] / response_times[1]
+    avg_wait = wait_times[0] / wait_times[1]
+    avg_turnaround = turnaround_times[0] / turnaround_times[1]
+    return avg_response, avg_wait, avg_turnaround
 def shortest_time_remaining_first(inputs: list):
     #lists are stored as: [running_sum, counter]
     response_times = [0, 0]
@@ -130,12 +185,17 @@ def main():
     # Test FCFS
     avg_resp, avg_wait, avg_turnaround = first_come_first_served(inputs)
     print(f"First Come, First Served")
-    print(f"Avg. Resp.: {avg_resp:.2f}, Avg. T.A.: {avg_turnaround:.2f}, Avg. Wait: {avg_wait:.2f}")
+    print(f"Avg. Resp.: {avg_resp:.2f}, Avg. T.A.: {avg_turnaround:.2f}, Avg. Wait: {avg_wait:.2f}\n")
+    
+    # Test SJF
+    avg_resp, avg_wait, avg_turnaround = shortest_job_first(inputs)
+    print(f"Shortest Job First")
+    print(f"Avg. Resp.: {avg_resp:.2f}, Avg. T.A.: {avg_turnaround:.2f}, Avg. Wait: {avg_wait:.2f}\n")
     
     # Test SRTF
     avg_resp, avg_wait, avg_turnaround = shortest_time_remaining_first(inputs)
     print(f"Shortest Remaining Time First")
-    print(f"Avg. Resp.: {avg_resp:.2f}, Avg. T.A.: {avg_turnaround:.2f}, Avg. Wait: {avg_wait:.2f}")
+    print(f"Avg. Resp.: {avg_resp:.2f}, Avg. T.A.: {avg_turnaround:.2f}, Avg. Wait: {avg_wait:.2f}\n")
 
 if __name__ == "__main__":
     main()
